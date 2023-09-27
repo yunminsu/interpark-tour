0fa6b6774a910771be74e4599b133c6ceabd144e
yunminsu <msg3265@nave.com>
2023-09-27 21:40:55 +0900

    업로드

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
 // ScrollMagic
-const spyEls = document.querySelectorAll('.scroll-spy');
-spyEls.forEach(function (spyEl) {
-    new ScrollMagic.Scene({
-      triggerElement: spyEl,
-      triggerHook: 0.8
-    })
-    .setClassToggle(spyEl, 'show')
-    .addTo(new ScrollMagic.Controller());
+// const spyEls = document.querySelectorAll('.scroll-spy');
+// spyEls.forEach(function (spyEl) {
+//     new ScrollMagic.Scene({
+//       triggerElement: spyEl,
+//       triggerHook: 0.8
+//     })
+//     .setClassToggle(spyEl, 'show')
+//     .addTo(new ScrollMagic.Controller());
+// });
+
+// 스크롤 시 navbar
+const navbarEl = document.querySelector('header .scroll-navbar');
+
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY);
+
+  if (window.scrollY > 200) {
+    navbarEl.style.display='block';
+  } else {
+    navbarEl.style.display='none';
+  }
 });
 
 // 상단배너 닫기
-
 const topbannerEl = document.querySelector('.top-banner');
 const topbannerdeleteBtn = document.querySelector('.top-banner-delete');
 
 topbannerdeleteBtn.addEventListener('click', function () {
     topbannerEl.classList.add('hide');
    });
+
+// 메인서치 제목 나타냄
+const fadeEl = document.querySelector('.main-section .fade');
+
+  gsap.to (fadeEl, 3, {
+    opacity: 1,
+  });
+
+
 
 // 배너 슬라이드
 new Swiper('.main-section .swiper', {
@@ -43,8 +64,15 @@ new Swiper('.main-section .swiper', {
 new Swiper('.toursale .swiper', {
   loop: true,
   disableOnInteraction: false,
-  slidesPerView: 3,
-  spaceBetween: 10,
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    }
+  },
   pagination: {
     el: '.toursale .swiper-pagination',
     clickable: true,
